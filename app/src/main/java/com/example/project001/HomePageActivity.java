1bed0e68eac731a9eff41e413f0d6216fabb2049
Cassian <Louliett@gmail.com>
2019-02-05 15:12:04 +0100

    suicide

--- app/src/main/java/com/example/project001/HomePageActivity.java
+++ app/src/main/java/com/example/project001/HomePageActivity.java
@@ -1,13 +1,36 @@
 package com.example.project001;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class HomePageActivity extends AppCompatActivity {
-
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Tab One");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Tab Two");
+        host.addTab(spec);
+
+        Intent startIntent = new Intent(getApplicationContext(), TestActivity.class);
+        startActivity(startIntent);
+
     }
 }
