13f50124ca4d560a1c757aac9ce8e2f31af13e90
Cassian <Louliett@gmail.com>
2019-02-05 16:12:52 +0100

    my new branch

--- app/src/main/java/com/example/project001/LoginActivity.java
+++ app/src/main/java/com/example/project001/LoginActivity.java
@@ -24,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("hehe");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
