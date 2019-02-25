9ec96d3d10dfa7451b32693a1fe34dd57ac8a906
MartinJjooga <jjoogam@gmail.com>
2019-02-25 03:21:07 +0100

    Fixed Gmail LogIn Error! Now we can Use our Emails to LogIn Onto Our App

--- app/src/main/java/com/example/project001/LoginActivity.java
+++ app/src/main/java/com/example/project001/LoginActivity.java
@@ -1,199 +1,117 @@
 package com.example.project001;
 
-import android.app.PendingIntent;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.support.v4.app.FragmentActivity;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
+
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.plus.Plus;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import DatabaseConnection.ThisIsForConnecting;
-
-public class LoginActivity extends FragmentActivity implements  ConnectionCallbacks,OnConnectionFailedListener,View.OnClickListener {
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.tasks.Task;
 
 
-    //variables
-    private TextView emailField;
-    private TextView passField;
-    private Button loginButton;
-    private TextView forgotPassword;
-    private TextView createAccount;
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignInTestActivity";
 
 
-//Implement constant variables
+    private static final int OUR_REQUEST_CODE = 49404;
 
-    private static final int SIGNED_IN = 0;
-    private static final int STATE_SIGNING_IN = 1;
-    private static final int STATE_IN_PROGRESS = 2;
-    private static final int RC_SIGN_IN = 0;
+    private GoogleSignInClient mGoogleApiClient;
 
-
-    private GoogleApiClient mGoogleApiClient;
-    private int mSignInProgress;
-    private PendingIntent mSignInIntent;
-
-    private SignInButton mSignInButton;
-    private Button mSignOutButton;
-    private Button mRevokeButton;
-    private TextView mStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mSignInButton = findViewById(R.id.sign_in_button);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = GoogleSignIn.getClient(this, gso);
 
 
-        mSignInButton.setOnClickListener(this);
+        ImageButton googlesignInButton = findViewById(R.id.sign_in_Button);
 
-
-
-
-        ThisIsForConnecting db = new ThisIsForConnecting();
-        db.ThisIsForConnecting1();
-
-    }
-    @Override
-    public void onClick(View v) {
-        if (!mGoogleApiClient.isConnecting()) {
-
-            switch (v.getId()) {
-                    case R.id.sign_in_button:
-                        resolveSignInError();
-
-
-                        break;
-
-                }
+        googlesignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
             }
-
+        });
 
 
     }
 
+
+    private void signIn() {
+        Intent signInIntent = mGoogleApiClient.getSignInIntent();
+        startActivityForResult(signInIntent,OUR_REQUEST_CODE);
+    }
+
+
     @Override
-    public void onConnected(Bundle bundle) {
-        mSignInButton.setEnabled(false);
-        // Indicate that the sign in process is complete.
-        mSignInProgress = SIGNED_IN;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == OUR_REQUEST_CODE) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+
         try {
-
-            String emailAddress = Plus.AccountApi.getAccountName(mGoogleApiClient);
-            mStatus.setText(String.format("Signed In to My App as %s", emailAddress));
-
-        } catch (Exception ex) {
-            String exception = ex.getLocalizedMessage();
-            String exceptionString = ex.toString();
-        }
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
 
+        } catch (ApiException e) {
 
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        mGoogleApiClient.connect();
-
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        if (mSignInProgress != STATE_IN_PROGRESS) {
-            mSignInIntent = connectionResult.getResolution();
-            if (mSignInProgress == STATE_SIGNING_IN) {
-                resolveSignInError();
-            }
-        }
-
-        onSignedOut();
-
-    }
-
-    private GoogleApiClient buildGoogleApiClient() {
-        return new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Plus.API, Plus.PlusOptions.builder().build())
-                .addScope(new Scope("email"))
-                .build();
-
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Drive.API)
-                .addScope(Drive.SCOPE_FILE)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
-    }
-
-    private void resolveSignInError() {
-
-
-        if (mSignInIntent != null) {
-            try {
-                mSignInProgress = STATE_IN_PROGRESS;
-                startIntentSenderForResult(mSignInIntent.getIntentSender(),
-                        RC_SIGN_IN, null, 0, 0, 0);
-
-                startActivity(new Intent(LoginActivity.this,SideBarActivity.class));
-
-
-
-
-            } catch (IntentSender.SendIntentException e) {
-                mSignInProgress = STATE_SIGNING_IN;
-                mGoogleApiClient.connect();
-
-            }
-        } else {
-
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
 
         }
 
 
-    }
-    private void onSignedOut() {
-        // Update the UI to reflect that the user is signed out.
-        mSignInButton.setEnabled(true);
+        Intent intent = new Intent(this, SideBarActivity.class);
+
+        startActivity(intent);
+
+
+
 
     }
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
