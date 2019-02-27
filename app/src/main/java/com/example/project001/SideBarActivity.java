2553cffead66a357ee287ff2354ffd00cc3562a0
Cassian <Louliett@gmail.com>
2019-02-27 14:27:55 +0100

    redjay

--- app/src/main/java/com/example/project001/SideBarActivity.java
+++ app/src/main/java/com/example/project001/SideBarActivity.java
@@ -40,13 +40,8 @@ public class SideBarActivity extends AppCompatActivity implements NavigationView
     String displayName;
     String Email;
 
-    LoginActivity loginActivity = new LoginActivity();
-
-
     GoogleSignInClient googleApiClient;
-
-
-
+    
 
 
     @Override
